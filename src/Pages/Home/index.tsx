e94f1245506200bd82fec1f79ceb8c94086ea698
arthurvenicio <stuks.midia@gmail.com>
2021-10-09 03:42:54 -0300

    Change index.tsx Page Home

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import * as S from "./style";
 
+import logo from "../../Assets/logo-netflix.svg";
+
 export function Home(): JSX.Element {
   return (
-    <S.Container>
-      <p>Teste</p>
-    </S.Container>
+    <>
+      <S.ContainerH>
+        <S.Header>
+          <S.Logo src={logo} alt="Netflix Logo" />
+          <S.Languages>
+            <option>Português</option>
+            <option>English</option>
+          </S.Languages>
+          <S.ButtonSignup>Entrar</S.ButtonSignup>
+        </S.Header>
+      </S.ContainerH>
+      <S.HighLight></S.HighLight>
+      <S.HighLight></S.HighLight>
+      <S.HighLight></S.HighLight>
+      <S.HighLight></S.HighLight>
+      <S.Questions></S.Questions>
+      <S.Footer></S.Footer>
+    </>
   );
 }
