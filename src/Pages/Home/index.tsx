221194038b5232eb39064ecd65b083528b0f1b71
arthurvenicio <stuks.midia@gmail.com>
2021-10-11 02:39:40 -0300

    Change index home page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -15,6 +15,21 @@ export function Home(): JSX.Element {
           </S.Languages>
           <S.ButtonSignup>Entrar</S.ButtonSignup>
         </S.Header>
+
+        <S.InfoBox>
+          <h1>Filmes, séries e muito mais. Sem limites.</h1>
+          <h2>Assista onde quiser. Cancele quando quiser.</h2>
+          <form>
+            <p>
+              Pronto para assistir? Informe seu email para criar ou reiniciar
+              sua assinatura.
+            </p>
+            <div className="email">
+              <input type="email" placeholder="Email" required />
+              <button type="submit">Vamos lá</button>
+            </div>
+          </form>
+        </S.InfoBox>
       </S.ContainerH>
       <S.HighLight></S.HighLight>
       <S.HighLight></S.HighLight>
